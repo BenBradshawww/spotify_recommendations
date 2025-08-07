88b992af1fb174984e805efda604ce7c8e843eab
ben <benbradshaw01@outlook.com>
2025-08-07 13:38:53 +0100

    Update update db logging

--- scripts/update_db.py
+++ scripts/update_db.py
@@ -3,19 +3,12 @@ import os
 import logging
 from logging.handlers import RotatingFileHandler
 
-sys.path.append(os.path.join(os.path.dirname(os.path.abspath(__file__)), 'upload_scripts'))
-
-from upload_recent_tracks_to_postgres import upload_recent_tracks_to_db
-from upload_playlists import upload_playlists
-from upload_track_meta_data import upload_track_meta_data
-from upload_album_meta_data import upload_album_meta_data
-from upload_artist_meta_data import upload_artist_meta_data
-
 
 # ----------------------------
 # Logging
 # ----------------------------
-logger = logging.getLogger(__name__)
+logger = logging.getLogger()
+logger.setLevel(logging.DEBUG)
 
 console_handler = logging.StreamHandler()
 console_handler.setLevel(logging.INFO)
@@ -36,6 +29,18 @@ file_handler.setFormatter(formatter)
 
 logger.addHandler(console_handler)
 logger.addHandler(file_handler)
+
+
+# ----------------------------
+# Import functions
+# ----------------------------
+sys.path.append(os.path.join(os.path.dirname(os.path.abspath(__file__)), 'upload_scripts'))
+
+from upload_recent_tracks_to_postgres import upload_recent_tracks_to_db
+from upload_playlists import upload_playlists
+from upload_track_meta_data import upload_track_meta_data
+from upload_album_meta_data import upload_album_meta_data
+from upload_artist_meta_data import upload_artist_meta_data
 
 
 # ----------------------------
